bad1abdcf41f62942da12d9f43b487f57041b861
Matthew Edwards <m.t.edwards410@gmail.com>
2021-10-05 11:26:44 +0000

    Create add_dev.html template

--- app.py
+++ app.py
@@ -98,6 +98,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_dev")
+def add_dev():
+    return render_template("add_dev.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
